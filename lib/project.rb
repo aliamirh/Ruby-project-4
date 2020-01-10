f7ac8eb37eeb9463e025bd6da5217a76031dac8d
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-10 11:28:35 -0800

    pass some tests

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,29 @@
 class Project
-attr_accessor :project_name
+attr_accessor :title, :id
 
 def initialize(attributes)
-  @project_name = attributes.fetch(:project_name)
+  @title = attributes.fetch(:title)
   @id = attributes.fetch(:id)
 
 end
+
+def self.all
+  returned_titles = DB.exec("SELECT * FROM projects;")
+  titles = []
+  returned_titles.each() do |title|
+    name = title.fetch("name")
+    id = title.fetch("id").to_i
+    titles.push(Project.new({:title => title, :id => id}))
+  end
+  titles
+end
+
+def save
+  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@titlee}') RETURNING id;")
+  @id = result.first().fetch("id").to_i
+end
+
+
 
 
 
