0ba552bace151fd736103c769e7803dad273c336
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-10 15:22:38 -0800

    passes test"

--- lib/project.rb
+++ lib/project.rb
@@ -31,6 +31,12 @@ def self.clear
   DB.exec("DELETE FROM projects *;")
 end
 
+def self.find(id)
+  project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+  title = project.fetch("title")
+  id = project.fetch("id").to_i
+  Project.new({:title => title, :id => id})
+end
 
 
 end
