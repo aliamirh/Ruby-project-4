6a45a3758c82327fc472dbf1d87d433a5556cedc
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-10 19:47:21 -0800

    changed routing and views

--- app.rb
+++ app.rb
@@ -12,5 +12,22 @@ also_reload('lib/**/*.rb')
 
 get('/') do
   @projects = Project.all
-  erb (:project)
+  erb(:home)
+end
+
+get('/home') do
+  @projects = Project.all
+  erv(:home)
+end
+
+get('/project/new') do
+  erb(:new_project)
+end
+
+post ('/home') do
+  title = params[:title]
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:home)
 end
