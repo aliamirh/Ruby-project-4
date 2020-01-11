0fdb8515e9e6787ad8e34ce91d6a91c5ffeb23c0
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-10 17:34:58 -0800

    add views

--- app.rb
+++ app.rb
@@ -1,3 +1,16 @@
-require("pg")
+
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+require('./lib/project')
+require('./lib/volunteer')
+require('pg')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
+
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @projects = Project.all
+  erb :home
+end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -30,7 +30,27 @@ def save
   @id = result.first().fetch("id").to_i
 end
 
+def self.find(id)
+volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+name = volunteer.fetch("name")
+project_id = volunteer.fetch("project_id").to_i
+id = volunteer.fetch("id").to_i
+Volunteer.new({:name => name, :project_id => project_id, :id => id})
+end
 
+def self.find_by_project(project_id)
+   volunteers = []
+   returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{project_id};")
+   returned_volunteers.each() do |volunteer|
+     name = volunteer.fetch("name")
+     id = volunteer.fetch("id").to_i
+     volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+   end
+   volunteers
+ end
 
+ def project
+   Volunteer.find(@project_id)
+ end
 
 end
