f76c437727ef8d688871ec57439a58bf401b4d8c
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-11 15:15:56 -0800

    add a large amount of updates, 6 hours worth

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Volunteer Tracker_
+
+#### _Set up a project and assign Volunteers to a project_
+
+#### By _**Ali Amirhamzeh**_
+
+## Description
+
+_This is a web site to manage various projects and assign volunteers to each project you created._
+
+## Setup/Installation Requirements
+
+* _clone/download repo_
+* _type "bundle install"_
+* _open web browser and type "localhost4567:"_
+* _follow instructions to set up a project_
+* _add volunteers to your project also_
+
+
+
+## Known Bugs
+
+_there is an issue when updating a volunteers name unfortunately_
+
+## Support and contact details
+
+_email me at example@email.com_
+
+## Technologies Used
+
+_RUBY, HTML, BOOTSTRAP, SINATRA_
+
+### License
+
+*Open source*
+
+Copyright (c) 2020 **_Ali Amirhamzeh_**
--- app.rb
+++ app.rb
@@ -12,22 +12,70 @@ also_reload('lib/**/*.rb')
 
 get('/') do
   @projects = Project.all
-  erb(:home)
+  erb(:projects)
 end
 
-get('/home') do
+get('/projects') do
   @projects = Project.all
-  erv(:home)
+  erb(:projects)
 end
 
-get('/project/new') do
+get('/projects/new') do
   erb(:new_project)
 end
 
-post ('/home') do
-  title = params[:title]
+post('/projects') do
+  title = params[:project_title]
   project = Project.new({:title => title, :id => nil})
-  project.save
+  project.save()
+  @projects = Project.all()
+  erb(:projects)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:project_title])
   @projects = Project.all
-  erb(:home)
+  erb(:projects)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteers)
+end
+
+post('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new({:name => params[:volunteer_name], :project_id => @project.id, :id => nil})
+  volunteer.save()
+  erb(:project)
+end
+
+patch ('/volunteers/:id') do
+     @volunteer = Volunteer.find(params[:id].to_i())
+     @volunteer.update(:name => params[:name])
+     redirect to ("/")
+end
+
+delete ('/volunteers/:id') do
+     @volunteer = Volunteer.find(params[:id].to_i())
+     @volunteer.delete
+     redirect to ("/")
 end
